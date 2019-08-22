ba9a311050b97fccd5660b005ca483669d0793ac
valkyrie7 <iamotley7@gmail.com>
2019-08-22 10:12:28 +0300

    stats file is a bit more clean. Data prep, analysis, and visualization is seperated.

--- code/ccn_stats.py
+++ code/ccn_stats.py
@@ -23,47 +23,52 @@
 
 # Import needed libraries and data
 
+import ccn_visualization
 import glob
 import json
 import os
 
-import matplotlib.pyplot as plt
 import numpy  as np
 import pandas as pd
 from scipy.stats import ttest_1samp
 from statsmodels.sandbox.stats.multicomp import multipletests
 
 
-def analyze(file, create_figures=True):
-    """Extract significant information from the json file."""
-
-    # <================================= Data ===============================>
+def read_and_prepare_data(file):
     # read data
     with open(file) as f:
         data = json.load(f)
 
-        # keys for json object
+    # keys for json object
     subjects = list(data.keys())
     windows = list(data[subjects[0]].keys())
 
     # information storage
-    info_storage = {}
+    eeg_sliced = {}
     for each in windows:
-        info_storage[each] = []  # initialization with empty list
+        eeg_sliced[each] = []  # initialization with empty list
 
     # store info
     for window in windows:
         for subject in subjects:
-            info_storage[window].append(data[subject][window])
+            eeg_sliced[window].append(data[subject][window])
 
-    # <================================= Tests ================================>
-    # do t-test
+    avg_values = [[k, v] for k, v in data['avg_all'].items()]
+
+    return avg_values, eeg_sliced
+
+
+def analyze(avg_values, eeg_sliced):
+    """Extract significant information from the json file."""
+
+    windows = list(eeg_sliced.keys())
+
     ttest_values = []
     for window in windows:
-        ttest_values.append(ttest_1samp(info_storage[window][:-1], 0.333)) # Why [:-1] and not all of info storage
+        ttest_values.append(ttest_1samp(eeg_sliced[window], 0.333))  # Why [:-1] and not all of eeg_sliced
 
     # combine info
-    avg_values = [[k, v] for k, v in data['avg_all'].items()]
+    # avg_values = [[k, v] for k, v in data['avg_all'].items()]
     t_values = np.asarray(ttest_values)
     p_values = [float(i) for i in t_values[:, -1]]
 
@@ -76,10 +81,6 @@ def analyze(file, create_figures=True):
     data = pd.DataFrame(values)  # pandas
     data.columns = ['window', 'accuracy', 't-statistic', 'pvalue', 'truthfulness', 'new_p']
 
-    # <=========================== visualize ==================================>
-    plt.clf()
-    fig, ax = plt.subplots()
-
     # decide on time
     windows_val = [2 * (x - 100) for x in [int(wind_frame.strip('()').split(',')[0])
                                            for wind_frame in windows]]
@@ -87,29 +88,13 @@ def analyze(file, create_figures=True):
     # average values
     vals = [x[-1] for x in avg_values][:len(windows_val)]
 
-    ax.plot(windows_val, vals)
-
     # p significance
     sig_index = data[data['truthfulness'] == '1.0']['accuracy'].index.tolist()[:len(windows_val)]
 
-    if create_figures:
-        # print(windows_val)
-        ax.plot([windows_val[x] for x in sig_index], [vals[x] for x in sig_index],
-                linestyle='none', color='r', marker='o')
+    return sig_index, windows_val, vals
 
-        # show starting understanding and chance level
-        ax.axvline(x=0, color='black', alpha=0.5, linestyle='--', label='end of baseline period')
-        ax.axhline(y=0.33, color='red', alpha=0.5, label='chance level')
 
-        ax.legend(loc='upper right')
-        ax.set_title('Classification accuracies')
 
-        dir = os.path.dirname(file)  ## directory of file
-        fig.savefig(dir+'/avg_accuracy.png', bbox_inches='tight')
-        plt.clf()
-        plt.close()
-    else:
-        return sig_index, windows_val, vals
 
 def choose_from_options( list_options, replace_option = ""):
     """Printing options the easily for the user to use."""
@@ -142,37 +127,30 @@ def choose( folder_list):
         return
     
     else:
-        analyze(folders[subdir])
+        avg_vals, eeg_sliced = read_and_prepare_data(folders[subdir])
+        s, w, v = analyze(avg_vals, eeg_sliced)
+        dir = os.path.dirname(folders[subdir])  ## directory of file
+        ccn_visualization.visualize(dir, s, w, v)
+
 
 def overlaying_analysis(video_path, still_path):
     "Takes paths to results json of video and still input experiments, and overlays the significance analysis"
 
-    v_sig_index, v_windows_val, v_vals = analyze(video_path, False)
-    s_sig_index, s_windows_val, s_vals = analyze(still_path, False)
+    v_avg_vals, v_eeg_sliced = read_and_prepare_data(video_path)
+    v_sig_index, v_windows_val, v_vals = analyze(v_avg_vals, v_eeg_sliced)
 
-    fig, ax = plt.subplots()
-    ax.plot(v_windows_val, v_vals,'g', label="Video")
-    ax.plot(s_windows_val, s_vals,'b', label='Still')
+    s_avg_vals, s_eeg_sliced = read_and_prepare_data(still_path)
+    s_sig_index, s_windows_val, s_vals = analyze(s_avg_vals, s_eeg_sliced)
 
-    # print(windows_val)
-    ax.plot([v_windows_val[x] for x in v_sig_index], [v_vals[x] for x in v_sig_index],
-            linestyle="none", color='r', marker='o')
-    ax.plot([s_windows_val[x] for x in s_sig_index], [s_vals[x] for x in s_sig_index],
-            linestyle="none", color='r', marker='o')
+    if (v_windows_val == s_windows_val):
+        windows_val = s_windows_val
+    else:
+        raise Exception('Still and video window values are not equal')
 
-    # show starting understanding and chance level
-    ax.axvline(x=0, color='black', alpha=0.5, linestyle='--', label='end of baseline period')
-    ax.axhline(y=0.33, color='red', alpha=0.5, label='chance level')
+    return v_sig_index, s_sig_index, windows_val, v_vals, s_vals
 
-    ax.legend(loc='upper right')
-    ax.set_title('Classification accuracies')  # If we really want to, we can get
-    # the window size and shift from the data
-    dir = os.path.dirname(video_path)  ## directory of file
-    fig.savefig(dir + '/overlayed_avg_accuracy.png', bbox_inches='tight')
-    plt.clf()
-    plt.close()
+    # TODO: Ask what is the folder list and when it is necessary
 
-# TODO: Ask what is the folder list and when it is necessary
 
 def run_all(folder_list, overlaying=False):
 
@@ -180,28 +158,25 @@ def run_all(folder_list, overlaying=False):
     for each in folder_list:
         folders = glob.glob(each + "*/accuracy_results.json")
         for each_subdir in folders:
-            print(each_subdir)
-            #analyze(each_subdir)
+            #print(each_subdir)
+            avg_values, info_storage = read_and_prepare_data(each_subdir)
+            s, w, v = analyze(avg_values, info_storage)
+            dir = os.path.dirname(each_subdir)  ## directory of file
+            ccn_visualization.visualize(dir, s, w, v)
 
 def overlay_all(main_folder_path):
     # name of folders to take the information from
     folders = [name for name in os.listdir(main_folder_path) if os.path.isdir(main_folder_path + name)]
-
     for folder in folders:
         # create the file path
         file_pth = main_folder_path + folder + '/'
-
         # get all the files
         files = [file_pth+name for name in os.listdir(file_pth) if name.endswith(".json")]
-        overlaying_analysis(files[0],files[1])
+        v_sig_index, s_sig_index, windows_val, v_vals, s_vals = overlaying_analysis(files[0],files[1])
+        dir = os.path.dirname(files[0])
+        ccn_visualization.visualize_still_and_video(dir, '/overlayed_avg_accuracy.png', v_sig_index, s_sig_index, windows_val, v_vals,
+                                  s_vals)
 
-
-#folder_list = [ "/Users/huseyinelmas/Desktop/Experiments/Video/"]
-#choose(folder_list)
-#run_all(folder_list)
-#analyze("/Users/huseyinelmas/Desktop/Experiments/old/Video_accuracy_results.json")
-#overlaying_analysis("/Users/huseyinelmas/Desktop/Experiments/old/Video_accuracy_results.json",
-                   # "/Users/huseyinelmas/Desktop/Experiments/old/Still_accuracy_results.json")
-
-overlay_all("/Users/huseyinelmas/Desktop/Experiments/")
+if __name__ == "__main__":
+    overlay_all("/home/sena/Desktop/Experiments/")
 
--- code/ccn_visualization.py
+++ code/ccn_visualization.py
@@ -1,24 +1,44 @@
-# plotting
 import matplotlib.pyplot as plt
 
 
-def plot(x, y):
-    """
-		plot different graphs. 
-	"""
-    try:
-        plt.plot(x, y)
-        plt.show()
-    except Exception:
-        print("    Something went wrong! plot method did not work!\n")
+def visualize(dir, name, sig_index, windows_val, vals):
+    fig, ax = plt.subplots()
+    ax.plot(windows_val, vals)
+
+    ax.plot([windows_val[x] for x in sig_index], [vals[x] for x in sig_index],
+            linestyle='none', color='r', marker='o')
+
+    # show starting understanding and chance level
+    ax.axvline(x=0, color='black', alpha=0.5, linestyle='--', label='end of baseline period')
+    ax.axhline(y=0.33, color='red', alpha=0.5, label='chance level')
+
+    ax.legend(loc='upper right')
+    ax.set_title('Classification accuracies')
+
+    fig.savefig(dir + name, bbox_inches='tight')
+    plt.clf()
+    plt.close()
 
 
-# ------------------------------------
-# testing
-if __name__ == "__main__":
-    x = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-    y = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-    print("Trying plot()!\n")
-    content = plot(x, y)
-    if content != None:
-        print("    plot() works.\n")
+def visualize_still_and_video(dir, name, v_sig_index, s_sig_index, windows_val, v_vals, s_vals):
+    fig, ax = plt.subplots()
+    ax.plot(windows_val, v_vals, 'g', label="Video")
+    ax.plot(windows_val, s_vals, 'b', label='Still')
+
+    # print(windows_val)
+    ax.plot([windows_val[x] for x in v_sig_index], [v_vals[x] for x in v_sig_index],
+            linestyle="none", color='r', marker='o')
+    ax.plot([windows_val[x] for x in s_sig_index], [s_vals[x] for x in s_sig_index],
+            linestyle="none", color='r', marker='o')
+
+    # show starting understanding and chance level
+    ax.axvline(x=0, color='black', alpha=0.5, linestyle='--', label='end of baseline period')
+    ax.axhline(y=0.33, color='red', alpha=0.5, label='chance level')
+
+    ax.legend(loc='upper right')
+    ax.set_title('Classification accuracies')  # If we really want to, we can get
+    # the window size and shift from the data
+
+    fig.savefig(dir + name, bbox_inches='tight')
+    plt.clf()
+    plt.close()
